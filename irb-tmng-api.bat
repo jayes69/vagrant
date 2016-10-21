ceefe6a6f2c4cbc33c6a760f150d1d7d5eb6280e
Roland Netzsch <roland.netzsch@encode.moe>
2016-10-21 14:32:14 +0200

    Fix titles of irb-tmng

--- irb-tmng-api.bat
+++ irb-tmng-api.bat
@@ -1,7 +1,7 @@
 @echo off
 call settings
 
-call library :terminal_init TMNG-APP - Rails Console
+call library :terminal_init TMNG-APIv2 - Rails Console
 pushd %api_path%
 vagrant ssh -c "/usr/bin/env bash -c 'bundle exec rails c'"
 popd
