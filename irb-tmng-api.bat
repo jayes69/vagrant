39be0778e776d5463ae1f574fcf77af72ecd76bf
unknown <bellb@BILLIE.net-up.net>
2016-10-21 16:55:04 +0200

    Fixed API Rails console

--- irb-tmng-api.bat
+++ irb-tmng-api.bat
@@ -3,5 +3,5 @@ call settings
 
 call library :terminal_init TMNG-APIv2 - Rails Console
 pushd %api_path%
-vagrant ssh -c "/usr/bin/env bash -c 'bundle exec rails c'"
+vagrant ssh -c "/usr/bin/env bash -c 'cd /vagrant/ && bundle exec rails c'"
 popd
